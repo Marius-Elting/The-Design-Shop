247e70850483dcdfcc9db68ad6a14ffbe9b8cb6e
Marius-Supercode <marius.supercode@gmail.com>
2022-10-21 16:33:12 +0200

    added contact site

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+
+
+
+
+function submitContact(event) {
+    let myform = document.getElementById('myform');
+    myform.addEventListener('submit', function (evt) {
+        let error = false;
+        if (error) {
+            evt.preventDefault();
+        } else {
+
+            event.preventDefault();
+            let fistname = document.getElementById("firstname").value;
+            let lastname = document.getElementById("lastname").value;
+            let email = document.getElementById("email").value;
+            let msg = document.getElementById("textarea").value;
+            let check = document.getElementById("checkbox");
+            console.log(fistname);
+            console.log(lastname);
+            console.log(email);
+            console.log(msg);
+            console.log(check.checked);
+
+            if (window.confirm('We have recieved the following Message!')) {
+                window.location = "../../index.html";
+            }
+        }
+    });
+}
